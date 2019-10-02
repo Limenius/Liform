82556c3aedaa26d39a6fbbdaff226d06050bae46
James Barnard <james.r.barnard@gmail.com>
2019-10-02 14:27:35 +0100

    Output form help as description during transformation

--- tests/Limenius/Liform/Tests/Transformer/CommonTransformerTest.php
+++ tests/Limenius/Liform/Tests/Transformer/CommonTransformerTest.php
@@ -50,15 +50,74 @@ class CommonTransformerTest extends LiformTestCase
             ->add(
                 'firstName',
                 TextType::class,
-                ['liform' => ['description' => 'A word that references you in the hash of the world']]
+                ['liform' => ['description' => $description = 'A word that references you in the hash of the world']]
             );
         $resolver = new Resolver();
         $resolver->setTransformer('text', new StringTransformer($this->translator));
+
+        $this->translator->method('trans')
+            ->will($this->returnCallback(function ($description) {
+                return $description;
+            }));
         $transformer = new CompoundTransformer($this->translator, null, $resolver);
         $transformed = $transformer->transform($form);
 
         $this->assertTrue(is_array($transformed));
         $this->assertArrayHasKey('description', $transformed['properties']['firstName']);
+        $this->assertSame($description, $transformed['properties']['firstName']['description']);
+    }
+
+    public function testDescriptionFromFormHelp()
+    {
+        $form = $this->factory->create(FormType::class)
+            ->add(
+                'firstName',
+                TextType::class,
+                [
+                    'help' => $description = 'Some information for the field',
+                ]
+            );
+
+        $resolver = new Resolver();
+        $resolver->setTransformer('text', new StringTransformer($this->translator));
+        $this->translator->method('trans')
+            ->will($this->returnCallback(function ($description) {
+                return $description;
+            }));
+        $transformer = new CompoundTransformer($this->translator, null, $resolver);
+        $transformed = $transformer->transform($form);
+
+        $this->assertTrue(is_array($transformed));
+        $this->assertArrayHasKey('description', $transformed['properties']['firstName']);
+        $this->assertSame($description, $transformed['properties']['firstName']['description']);
+    }
+
+    public function testDescriptionFromFormHelpOverriddenByLiformDescription()
+    {
+        $form = $this->factory->create(FormType::class)
+            ->add(
+                'firstName',
+                TextType::class,
+                [
+                    'help' => $help = 'Some information for the field',
+                    'liform' => [
+                        'description' => $description = 'This will be set',
+                    ],
+                ]
+            );
+
+        $resolver = new Resolver();
+        $resolver->setTransformer('text', new StringTransformer($this->translator));
+        $transformer = new CompoundTransformer($this->translator, null, $resolver);
+        $this->translator->method('trans')
+            ->will($this->returnCallback(function ($description) {
+                return $description;
+            }));
+        $transformed = $transformer->transform($form);
+
+        $this->assertTrue(is_array($transformed));
+        $this->assertArrayHasKey('description', $transformed['properties']['firstName']);
+        $this->assertSame($description, $transformed['properties']['firstName']['description']);
     }
 
     public function testLabel()
